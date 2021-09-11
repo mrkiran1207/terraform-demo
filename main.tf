f62d2061a2606a1732ea206eb976a621508c3aa2
mrkiran1207 <m.rkiran@hotmail.com>
2021-09-11 13:33:55 +0530

    Update main.tf
    updated azure basic strings

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
-  features {}
+		subscription_id = "3fd51abc-3324-4b73-ba94-b31523517c41"
+		client_id = "4d9ff00d-7ec4-49cc-a83c-e46add9e300f"
+		client_secret = "PsTE8~J63Eut-zt9B3nOP.3901W_b3wFO9"
+		tenant_id = "531b69ad-d219-4725-a625-9a26ca1f167e" 
 }
 
 resource "azurerm_resource_group" "resource_group_terraform" {
